b721a06ae97bf365ff4af92f868bb85358e64eb4
Arianna Johnson <ariannajohnson588@gmail.com>
2021-03-24 11:56:06 -0400

    problem 1

--- 01-big-octopus.js
+++ 01-big-octopus.js
@@ -1,8 +1,29 @@
+// Find the longest fish in O(n^2) time.
+// Do this by comparing all fish lengths to all other fish lengths
+
+// 0(n^2) time
+// O(1) space
 function quadraticBiggestFish(fishes) {
-  // Code goes here ...
+  for (let i = 0; i < fishes.length; i++) {
+    let fish1 = fishes[i];
+    let maxLength = true;
+    for (let j = i; j < fishes.length; j++) {
+      let fish2 = fishes[j];
+      if (fish2.length > fish1.length) maxLength = false;
+    }
+    if (maxLength) return fish1;
+  }
 }
 
+const fishies = ['fish', 'fiiish', 'fiiiiish', 'fiiiish', 'fffish',
+'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh'];
 
+console.log(quadraticBiggestFish(fishies));
+
+
+//Find the longest fish in O(n log n) time.
+//Hint: Use the built in Array.sort() method which runs in O(n log n).
+//Remember that Big O is classified by the dominant term.
 function nlognBiggestFish(fishes) {
   // Code goes here ...
 }
