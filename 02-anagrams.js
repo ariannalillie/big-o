972a6291dbc15f15cb2bc844b6f9a851219fbae2
Arianna Johnson <ariannajohnson588@gmail.com>
2021-04-21 17:52:26 -0400

    stuff

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -6,6 +6,8 @@ function firstAnagram(str1, str2) {
 
   for (let letter of arr1) {
     let index = arr2.indexOf(letter)
+    // The indexOf() method returns the first index at which
+    // a given element can be found in the array, or -1 if it is not present.
     if (index === -1) return false;
     // if we didn't return false, we found a match
     // so remove letter from array
@@ -30,6 +32,18 @@ function secondAnagram(str1, str2) {
 
 console.log(secondAnagram("elvis", "lives"));
 
+
+// This time, use two Objects to store the number of times each letter appears in both words.
+// Compare the resulting Objects.
+
+// O(n) linear time
+// O(1) constant space
+// Here, the intuitive answer to the space complexity is
+// O(n) because we're adding a separate key in the object
+// for each character. But if the keys in the object are single
+// characters, then how many different keys can we have?
+// How many different chars in the alphabet? A constant number
+// (26 + numbers and symbols for English alphabet).
 function thirdAnagram(str1, str2) {
   // Code goes here ....
 }
@@ -38,3 +52,4 @@ function thirdAnagram(str1, str2) {
 function fourthAnagram(str1, str2) {
   // Code goes here ....
 }
+
--- 03-two-sum.js
+++ 03-two-sum.js
@@ -1,12 +1,27 @@
+//O(n^2) quadratic time
+//O(1) constant space
 function badTwoSum(arr, targetSum) {
-  // Code goes here ...
+  for (let i = 0; i < arr.length - 1; i++) {
+    let num1 = arr[i];
+    for(let j = i + 1; j < arr.length; j++) {
+      let num2 = arr[j];
+      if ((num1 + num2) === targetSum) return true
+    }
+  }
+  return false;
 }
 
+const arr = [0, 1, 5, 7];
 
+
+//O(nlogn) linearithmic time
+//O(1) linear space
 function okayTwoSum1(arr, targetSum) {
-  // Code goes here ...
+  arr.sort((a, b) => a - b);
+  console.log(arr)
 }
 
+console.log(okayTwoSum1(arr, 5));
 
 function okayTwoSum2(arr, targetSum) {
   // Code goes here ...
